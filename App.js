a1f88d34a4d8b38cf727b3a442f159c50c174641
Lam Ngo <lam.ngo.qu@gmail.com>
2018-02-14 17:16:07 +0200

    Added Speech to text

--- App.js
+++ App.js
@@ -3,49 +3,43 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-//USE IBM WATSON TONE ANALYZER LIBRARY
-const ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+//USE IBM WATSON SPEECH TO TEXT LIBRARY
+var SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
+var fs = require('fs');
 
-const tone_analyzer = new ToneAnalyzerV3({
-  username: '47e7648f-3796-41da-93be-ff64fafa7b81',
-  password: 'muqL70UErLlZ',
-  version_date: '2018-02-12'
+const speechToText = new SpeechToTextV1({
+  username: '15983ad1-eeff-4d76-8ee6-099078ea7995',
+  password: 'oJTqqceoksvR',
+  url: 'https://stream.watsonplatform.net/speech-to-text/api/'
 });
 
-//GET REQUEST HANDLER TO ANALYZING TEST DATA
-app.get('/get', function(req, res){
-  console.log('GET request!');
+const getText = function(path, res){
+  console.log('Speech to text from path: ' + path);
 
   var params = {
-    'tone_input': require('./data_test/tone.json'),
-    'content_type': 'application/json'
+    // From file
+    audio: fs.createReadStream(path),
+    content_type: 'audio/flac'
   };
 
-  tone_analyzer.tone(params, function(error, response) {
-    if (error)
-      res.send("Error: " + error);
-    else
-      res.send(JSON.stringify(response, null, 2));
-    }
-  );
-});
-
-//POST REQUEST HANDLER FOR ANALYZING USER INPUT DATA
-app.post('/post', function(req, res) {
-  console.log(req.body);
-
-  var params = {
-    'tone_input': req.body,
-    'content_type': 'application/json'
-  };
-
-  tone_analyzer.tone(params, function(error, response) {
+  speechToText.recognize(params, function(error, response) {
     if (error)
       res.send(error);
     else
       res.send(JSON.stringify(response, null, 2));
-    }
-  );
-})
+  });
+}
+
+//GET REQUEST HANDLER FOR TONE
+app.get('/get/tone/houseburning', function(req, res){
+  console.log('GET request!');
+  getText('data_test/houseburning.flac', res);
+});
+
+app.get('/get/tone/manwithgun', function(req, res){
+  console.log('GET request!');
+  getText('data_test/manwithgun.flac', res);
+});
+
 
 app.listen(3005);
