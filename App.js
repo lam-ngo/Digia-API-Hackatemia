ca351306b1f3fe45770a48c81114c3af48204726
Lam Ngo <lam.ngo.qu@gmail.com>
2018-02-14 18:30:04 +0200

    final review

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ const speechToText = new SpeechToTextV1({
   url: 'https://stream.watsonplatform.net/speech-to-text/api/'
 });
 
-const getText = function(path, res){
+const getTextForTone = function(path, res){
   console.log('Speech to text from path: ' + path);
 
   var params = {
@@ -30,18 +30,44 @@ const getText = function(path, res){
   });
 }
 
+const getTextForQA = function(path, res){
+  console.log('Speech to text from path: ' + path);
+
+  var params = {
+    // From file
+    audio: fs.createReadStream(path),
+    content_type: 'audio/flac'
+  };
+
+  speechToText.recognize(params, function(error, response) {
+    if (error)
+      res.send(error);
+    else
+      QA({question: response.results[0].alternatives[0].transcript}, res);
+  });
+}
+
 //GET REQUEST HANDLER FOR TONE
 app.get('/get/tone/houseburning', function(req, res){
-  console.log('GET request!');
-  getText('data_test/houseburning.flac', res);
+  console.log('GET request for tone: houseburning');
+  getTextForTone('data_test/houseburning.flac', res);
 });
 
 app.get('/get/tone/manwithgun', function(req, res){
-  console.log('GET request!');
-  getText('data_test/manwithgun.flac', res);
+  console.log('GET request for tone: manwithgun');
+  getTextForTone('data_test/manwithgun.flac', res);
 });
 
+//GET REQUEST HANDLER FOR QA
+app.get('/get/qa/houseburning', function(req, res){
+  console.log('GET request for qa: houseburning');
+  getTextForQA('data_test/houseburning.flac', res);
+});
 
+app.get('/get/qa/manwithgun', function(req, res){
+  console.log('GET request for qa: manwithgun');
+  getTextForQA('data_test/manwithgun.flac', res);
+});
 
 //USE IBM WATSON TONE ANALYZER LIBRARY
 const ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
@@ -70,8 +96,29 @@ const toneAnalyze = function(json, res){
 };
 
 //USE MICROSOFT QA MAKER
+var request = require('request');
+
 const QA = function(json, res){
-  
+
+  var options = {
+    method: 'POST',
+    url: 'https://westus.api.cognitive.microsoft.com/qnamaker/v1.0/knowledgebases/2062567b-201e-41cc-848e-79522cbbe4c0/generateAnswer',
+    headers: {
+      'Ocp-Apim-Subscription-Key': '931057a3c86c4a64a34ac15de9f1b2f9',
+      'Content-Type': 'application/json',
+      'Cache-Control': 'no-cache'
+    },
+    body: JSON.stringify(json)
+  };
+
+  function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var info = JSON.parse(body);
+      res.send(info);
+    }
+  }
+
+  request(options, callback);
 }
 
 app.listen(3005);
